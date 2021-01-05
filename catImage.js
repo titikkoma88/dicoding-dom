488fbcccb529755921655f95e6f720828025e383
titikkoma88 <jie.koma88@gmail.com>
2021-01-05 20:22:21 +0700

    Initial commit

---
+++ catImage.js
@@ -0,0 +1,23 @@
+console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
+
+//script dibawah ini di copy ke console
+
+// let catImage = document.querySelector("#catImage");
+// catImage.setAttribute("src", "https://i.ibb.co/55VG7vL/three-cat.jpg");
+
+// catImage.getAttribute("src");
+
+// let caption = document.querySelector("#caption");
+// caption.innerHTML = '<em>Tiga Anak Kucing</em>'
+
+// caption.innerText = "<em>Tiga Anak Kucing</em>";
+
+// let newElement = document.createElement('p');
+
+// newElement.innerHTML = 'Anda menekan gambar kucing sebanyak <span id="count">0</span> kali';
+
+// document.body.appendChild(newElement);
+
+// catImage.addEventListener('click', function(event) {
+//     document.querySelector('#count').innerText++;
+// });
